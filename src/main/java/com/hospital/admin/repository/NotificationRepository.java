d3fe404bfee7d6e20146782183a4313ffb7f305a
sinchangowda234 <sinchangowda234@gmail.com>
2026-03-23 01:10:47 +0530

    Deleted repository

--- src/main/java/com/hospital/admin/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hospital.admin.repository;
-
-import com.hospital.admin.entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
